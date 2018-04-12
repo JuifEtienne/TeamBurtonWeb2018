aece1a76259f603ecefd5cfcbf82795713c69994
Julian <bruxelle.julian@wanadoo.fr>
2018-04-12 17:10:03 +0200

    Also added routes for created classes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -20,7 +20,7 @@ $router->get( '/hello/world' , function () {
 	return "Hello World" ;
 });
 
-// Exemple GET avec paramètre et middleware (TP PASCALE) 
+// Exemple GET avec paramètre et middleware (TP PASCALE)
 $router->get( '/hello/{name}' , [ 'middleware' => 'hello' , function ($name) {
 	return "Hello {$name}" ;
 }]);
@@ -28,13 +28,55 @@ $router->get( '/hello/{name}' , [ 'middleware' => 'hello' , function ($name) {
 $router->group(['prefix' => 'voyages'], function($app)
 {
 	$app->get('/','VoyageController@getAll');
-	
+
 	$app->get('/{id}','VoyageController@getVoyage');
 
 	$app->post('/add','VoyageController@createVoyage');
 
 	$app->put('update/{id}','VoyageController@updateVoyage');
- 	 
+
 	$app->delete('delete/{id}','VoyageController@deleteVoyage');
+
+});
+
+$router->group(['prefix' => 'bagages'], function($app)
+{
+	$app->get('/','BagageController@getAll');
+
+	$app->get('/{id}','BagageController@getBagage');
+
+	$app->post('/add','BagageController@createBagage');
+
+	$app->put('update/{id}','BagageController@updateBagage');
+
+	$app->delete('delete/{id}','BagageController@deleteBagage');
+
+});
+
+$router->group(['prefix' => 'objets'], function($app)
+{
+	$app->get('/','ObjetController@getAll');
+
+	$app->get('/{id}','ObjetController@getObjet');
+
+	$app->post('/add','ObjetController@createObjet');
+
+	$app->put('update/{id}','ObjetController@updateObjet');
+
+	$app->delete('delete/{id}','ObjetController@deleteObjet');
+
+});
+
+$router->group(['prefix' => 'monnaies'], function($app)
+{
+	$app->get('/','MonnaieController@getAll');
+
+	$app->get('/{id}','MonnaieController@getMonnaie');
+
+	$app->post('/add','MonnaieController@createMonnaie');
+
+	$app->put('update/{id}','MonnaieController@updateMonnaie');
+
+	$app->delete('delete/{id}','MonnaieController@deleteMonnaie');
 
 });
