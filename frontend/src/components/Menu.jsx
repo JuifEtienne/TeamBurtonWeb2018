93b1e900ea1417610fcf2778d80d906689b51977
Ugo Bouveron <ugobouveron@gmail.com>
2018-04-17 19:04:11 +0200

    Menu template
    CSS rules for the menu + JSX template. Now it needs to be filled dynamically with the rigth data. It would need something like a router component.

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import styles from '../assets/sass/menu.scss'
 
 export default class Menu extends React.Component {
   render() {
     return (
-     <nav>
-        <p>This is the Menu.</p>
-      </nav>
+     <div>  
+        <img src={require('../assets/img/logo.svg')} alt='logo'/>            
+        <h2>Journeo</h2>
+            <nav className='nav' role='navigation'>
+                <ul className='menu'>
+                    <li>
+                        <a>
+                            <div>icone</div>
+                            <div>Home</div>
+                        </a>
+                    </li>
+                    <li><a>
+                            <div>icone</div>
+                            <div>1st trip</div>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        <p>© 2018 - Team Burton</p>
+      </div>
+    
     );
   }
 }
